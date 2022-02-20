f9ca75071a71332dd9a004ed79d310c8b1950da8
NAVentilateN <valentinetan91@gmail.com>
2022-02-20 17:08:33 +0800

    added more test to post model

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -8,13 +8,34 @@ class PostsTest < ApplicationSystemTestCase
 
   test "Ensure all post are being displayed" do
     visit posts_url
+
     assert_selector ".postlist"
-    assert_selector "h2", count: Post.count
+    assert_selector ".post", count: Post.count
   end
 
   test "lets a signed in user see all post" do
+    visit "/"
+    # save_and_open_screenshot
     login_as users(:test_user)
     visit "/posts"
+    # save_and_open_screenshot
     assert_selector "h1", text: "Post"
+    assert_selector ".post", count: Post.count
+  end
+
+  test "Should not save if post is more than 140 character" do
+    post = Post.new(description: "a" * 141)
+    assert_not post.save
+  end
+
+  test "Should not save if post is less than or equal to 140 character but does not have a user" do
+    post = Post.new(description: "a" * 140)
+    assert_not post.save
+  end
+
+  test "Should save if post is less than or equal to 140 character and have a user" do
+    post = Post.new(description: "a" * 140)
+    post.user = users(:test_user)
+    assert post.save
   end
 end
