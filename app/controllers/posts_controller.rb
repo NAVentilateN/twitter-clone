e90c814fee8e933eeb8c6aa922fbe7d2fce92c36
NAVentilateN <valentinetan91@gmail.com>
2022-02-23 21:50:54 +0800

    complete advance setup to conduct test for application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,16 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.neq
+    @post = Post.new
   end
 
   def create
-    @post = Post.new
+    @post = Post.new(post_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,10 +23,19 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
   end
 
   def delete
     @post = Post.find(params[:id])
     @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description)
   end
 end
