f3f5e617197e3873bfc3155c331bc70a3e81dac2
NAVentilateN <valentinetan91@gmail.com>
2022-03-05 17:43:47 +0800

    use SSL and the pouma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -31,13 +31,24 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
+
+# port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+
+  ActiveRecord::Base.establish_connection
+end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
