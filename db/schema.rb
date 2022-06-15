f065e3d147c601fe46a7484d82f46916749aa08f
Tan Valentine <valentinetan91@gmail.com>
2022-06-16 01:57:17 +0800

    add active-storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_063943) do
+ActiveRecord::Schema.define(version: 2022_06_15_175100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 2022_06_04_063943) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "friendlists", "users"
   add_foreign_key "friends", "users"
   add_foreign_key "posts", "users"
